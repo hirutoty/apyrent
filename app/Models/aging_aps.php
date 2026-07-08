bb2d06beaf3b37205c5fd29f1ffb35ce7251e697
hirutoty <hirutotiriawan@gmail.com>
2026-07-08 11:54:29 +0700

    tambah console

--- app/Models/aging_aps.php
+++ app/Models/aging_aps.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Aging_aps extends Model
+class aging_aps extends Model
 {
     use HasFactory;
 
